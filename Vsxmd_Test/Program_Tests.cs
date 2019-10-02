8a25b1d707ba46fb668f444a0315e35fd08417cb
MddMBorg <destructivevulcan@gmail.com>
2019-10-02 21:55:21 +0100

    Initial commit after fork: playing around

---
+++ Vsxmd_Test/Program_Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vsxmd_Test
+{
+    [TestClass]
+    public class Program_Tests
+    {
+        [TestMethod]
+        public void Main_Test()
+        {
+            Vsxmd.Converter converter = new Vsxmd.Converter(System.Xml.Linq.XDocument.Load("TestResources\\vsxmd.xml"));
+            Console.Write(1);
+        }
+    }
+}
---
+++ Vsxmd_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Vsxmd_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Vsxmd_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02c3fad7-9180-4f20-8ce2-998fa439832e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
