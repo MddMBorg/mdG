4a4ba2e03e9fc27aa685ec2c17c5cae3c2786a09
MddMBorg <destructivevulcan@gmail.com>
2019-10-19 22:56:04 +0100

    Installer now "works"

--- mdGInstallerActions/InstallerActions.cs
+++ mdGInstallerActions/InstallerActions.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -22,20 +23,21 @@ namespace mdGInstallerActions
         {
             base.Install(stateSaver);
 
-            string path = Environment.GetEnvironmentVariable("PATH");
+            string pathVars = Environment.GetEnvironmentVariable("PATH");
             string installDir = Context.Parameters["path"].TrimEnd('\\');
             string separator = Path.DirectorySeparatorChar.ToString();
             installDir += installDir.EndsWith(separator, StringComparison.OrdinalIgnoreCase) ? separator : "";
 
-            if (!path.Contains(installDir))
-                Environment.SetEnvironmentVariable("PATH", $"{path};{installDir}", EnvironmentVariableTarget.Machine);
+            if (!pathVars.Contains(installDir))
+                Environment.SetEnvironmentVariable("PATH", $"{pathVars};{installDir}", EnvironmentVariableTarget.Machine);
 
             MessageBox.Show(installDir);
-            MessageBox.Show(path);
+            MessageBox.Show(pathVars);
 
+            if (File.Exists(Path.Combine(installDir, "mdGExtension.vsix")))
+                Process.Start(Path.Combine(installDir, "mdGExtension.vsix"));
 
         }
-
 
     }
 
