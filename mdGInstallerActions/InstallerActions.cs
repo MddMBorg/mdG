b3aebcb7445df604a89a97a8c03dd5be22d024a9
MddMBorg <destructivevulcan@gmail.com>
2019-10-19 22:23:23 +0100

    Oof

---
+++ mdGInstallerActions/InstallerActions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace mdGInstallerActions
+{
+    [RunInstaller(true)]
+    public partial class InstallerActions : Installer
+    {
+        public InstallerActions()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+
+            string path = Environment.GetEnvironmentVariable("PATH");
+            string installDir = Context.Parameters["path"].TrimEnd('\\');
+            string separator = Path.DirectorySeparatorChar.ToString();
+            installDir += installDir.EndsWith(separator, StringComparison.OrdinalIgnoreCase) ? separator : "";
+
+            if (!path.Contains(installDir))
+                Environment.SetEnvironmentVariable("PATH", $"{path};{installDir}", EnvironmentVariableTarget.Machine);
+
+            MessageBox.Show(installDir);
+            MessageBox.Show(path);
+
+
+        }
+
+
+    }
+
+}
